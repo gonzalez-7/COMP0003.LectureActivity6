a105445692669007a330533a4b96971ed03f8ff4
gonzalez-7 <20agonzalezp@gmail.com>
2025-04-28 18:52:39 -0700

    Created secrets.json and Created the WebDevAcademyContext

---
+++ COMP0003.LectureActivity6/Data/WebDevAcademyContext.cs
@@ -0,0 +1,15 @@
+﻿using COMP0003.LectureActivity6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP0003.LectureActivity6.Data
+{
+	public class WebDevAcademyContext : DbContext
+	{
+		public WebDevAcademyContext(DbContextOptions<WebDevAcademyContext> options) : base(options)
+		{ 
+		}
+		public DbSet<Student> Students { get; set; }
+		public DbSet<Course> Courses { get; set; }
+		public DbSet<Enrollment> Enrollments { get; set; }
+	}
+}
--- COMP0003.LectureActivity6/Program.cs
+++ COMP0003.LectureActivity6/Program.cs
@@ -1,36 +1,37 @@
-namespace COMP0003.LectureActivity6
+namespace COMP0003.LectureActivity6;
+using COMP0003.LectureActivity6.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapStaticAssets();
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-                .WithStaticAssets();
-
-            app.Run();
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapStaticAssets();
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}")
+            .WithStaticAssets();
+
+        app.Run();
     }
 }
