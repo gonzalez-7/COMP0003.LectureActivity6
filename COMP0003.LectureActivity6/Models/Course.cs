959f250072b3ebe63ddd95484fce6af6a26ec2bb
gonzalez-7 <20agonzalezp@gmail.com>
2025-04-28 18:38:02 -0700

    Added Models for Student, Course, and Enrollment Entities.

---
+++ COMP0003.LectureActivity6/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP0003.LectureActivity6.Models
+{
+	public class Course
+	{
+		public int CourseId { get; set; }
+		[Required]
+		public string Title { get; set; }
+
+		//Collection navigation property
+
+		public virtual ICollection<Enrollment> Enrollments { get; set; }
+	}
+}
--- COMP0003.LectureActivity6/Models/Student.cs
+++ COMP0003.LectureActivity6/Models/Student.cs
@@ -1,6 +1,17 @@
-﻿namespace COMP0003.LectureActivity6.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP0003.LectureActivity6.Models
 {
 	public class Student
 	{
+		public int StudentId { get; set; }
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+		//Collection navigation property
+
+		public virtual ICollection<Enrollment>? Enrollments { get; set; }
 	}
 }
